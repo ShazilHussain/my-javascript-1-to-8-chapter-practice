995e008543830e48e612b3d70a80e9ea9c444923
ShazilHussain <116807199+ShazilHussain@users.noreply.github.com>
2022-10-31 17:11:49 +0500

    my javascript 1 to 8 practice
    my javascript 1 to 8 practice

---
+++ app.js
@@ -0,0 +1,51 @@
+// chapter 1: practice.
+alert("syed Shazil Hussain Zaidi");
+// chapter complete
+
+// chaper 2: practice.
+var father_name ;
+father_name = "Syed asif Hussain Zaidi";
+// chapter complete
+
+// chapter 3: practice.
+var number = 786;
+var second_number = 456;
+// chapter complete
+
+// chapter 4: practice.
+var username = "shazil zaidi" // legal var
+
+var &username = "shazil zaidi" // illegal var
+
+var $username = "shazil zaidi" // legal var
+
+var !username = "shazil zaidi" // illegal var
+
+var username3 = "shazil zaidi" // legal var
+// chapter complete
+
+// chapter 5: practice.
+var mathematice_adition = 45 + 54
+var mathematice_subraction = 45 - 54
+var mathematice_multiplication = 45 * 54
+var mathematice_division = 45 / 54
+// chapter complete
+
+// chapter 6: practice.
+var pass_increment = 2 ;
+pass_increment = ++pass_increment
+var num = 3
+num = --num
+// chapter complete
+
+// chapter 7: practice.
+var bodmas_rule = 43 + (67 - 76)
+// chapter complete
+
+// chapter 8: practice.
+var wellcome = "wellcome student" 
+var user_name = "Shazil Hussain Zaidi" 
+var duble_fulstop = ":"
+var full_sentence = wellcome + " " + user_name + duble_fulstop
+alert(full_sentence) 
+// chapter complete
